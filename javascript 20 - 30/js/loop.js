43f578525d24f5a4196e72ba259957e5670fa24b
Muhammad Falah <110474441+MuhammadFalahKresnatra@users.noreply.github.com>
2023-01-11 20:02:40 +0700

    Add files via upload

---
+++ javascript 20 - 30/js/loop.js
@@ -0,0 +1,36 @@
+// Loop Menggunakan While
+
+let mulai = 1;
+let selesai = 10;
+
+while (mulai <= selesai) {
+    console.log(mulai);
+
+    mulai++;
+}
+
+// Loop Menggunakan Do While
+
+let start = 101;
+let finish = 110;
+
+do {
+    console.log(start);
+    start++
+} while (start < finish);
+
+// Loop Menggunakan For
+
+let awal = 1;
+let akhir = 10;
+
+for (awal; awal < akhir; awal++ ) {
+    console.log(awal);
+}
+
+console.log("------------------");
+
+for (let i = 1; i < 10; i++) {
+    console.log(i);
+    
+}
